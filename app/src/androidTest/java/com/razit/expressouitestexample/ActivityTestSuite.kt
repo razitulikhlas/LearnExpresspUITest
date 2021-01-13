01e4369e6e9e66f98639acf800a3d3238d46b300
razitul ikhlas <razituli@gmail.com>
2021-01-13 22:35:44 +0700

    suite test

---
+++ app/src/androidTest/java/com/razit/expressouitestexample/ActivityTestSuite.kt
@@ -0,0 +1,13 @@
+package com.razit.expressouitestexample
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+    MainActivityTest::class,
+    SecondActivityTest::class
+)
+class ActivityTestSuite{
+
+}
--- app/src/androidTest/java/com/razit/expressouitestexample/SecondActivityTest.kt
+++ app/src/androidTest/java/com/razit/expressouitestexample/SecondActivityTest.kt
@@ -1,5 +1,6 @@
 package com.razit.expressouitestexample
 
+import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions
@@ -16,5 +17,34 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class SecondActivityTest{
+    @get:Rule
+    val activityRule = ActivityScenarioRule(SecondActivity::class.java)
+
+    @Test
+    fun testActivity_inView() {
+
+        onView(withId(R.id.secondary))
+            .check(matches(isDisplayed()))
+
+        // Notice this does not effect the next test
+        activityRule.scenario.moveToState(Lifecycle.State.DESTROYED)
+    }
+
+    // Visibility
+    @Test
+    fun testVisibility_title_nextButton() {
+        onView(withId(R.id.activity_secondary_title))
+            .check(matches(isDisplayed()))
+
+        onView(withId(R.id.button_back))
+            .check(matches(isDisplayed()))
+    }
+
+    // Text
+    @Test
+    fun testTitleTextDisplayed() {
+        onView(withId(R.id.activity_secondary_title))
+            .check(matches(withText(R.string.text_secondaryactivity)))
+    }
 
 }
